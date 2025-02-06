429967d40d0cb241f47bc239077a3f3f7cedcc90
Arsalan Parham <parham_arsalan@yahoo.com>
2025-02-06 17:28:46 +0100

    Fix proxy issue

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,20 @@ export default defineConfig({
   plugins: [react(), 
     tailwindcss()],
 
-  server: {
-    proxy: {
-      '/login': { // Proxy requests to /login to your backend
-        target: 'http://85.9.97.246:8080',
-        changeOrigin: true, // Essential for CORS
-        withCredentials: true, // Important for forwarding cookies
-        },
-        '/panel/api/inbounds/getClientTraffics': { 
-        target: 'http://85.9.97.246:8080',
-        changeOrigin: true, // Essential for CORS
-        withCredentials: true, // Important for forwarding cookies
-        }
-      }
-    }
+  // server: {
+  //   proxy: {
+  //     '/login': { // Proxy requests to /login to your backend
+  //       target: 'http://85.9.97.246:8080',
+  //       changeOrigin: true, // Essential for CORS
+  //       withCredentials: true, // Important for forwarding cookies
+  //       },
+  //       '/panel/api/inbounds/getClientTraffics': { 
+  //       target: 'http://85.9.97.246:8080',
+  //       changeOrigin: true, // Essential for CORS
+  //       withCredentials: true, // Important for forwarding cookies
+  //       }
+  //     }
+  //   }
   })
 
 
