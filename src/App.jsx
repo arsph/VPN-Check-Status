c927a801f5425c0ee5d4f234ecb488c1a0bd2a7e
Arsalan Parham <parham_arsalan@yahoo.com>
2025-05-22 19:48:50 +0200

    Updated with HTTPS from localhost

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css';
+import Inputlink from "./components/Inputlink.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Inputlink/>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:5001',
+  //   }
+  // },
+
+  server: {
+    proxy: {
+      '/login': {
+        target: 'https://85.9.97.246:443',
+        changeOrigin: true,
+        withCredentials: true,
+        secure: false,
+      },
+      '/panel/api/inbounds/getClientTraffics': {
+        target: 'https://85.9.97.246:443',
+        changeOrigin: true,
+        withCredentials: true,
+        secure: false,
+      },
+    },
+  },
+  
+  headers: {
+    'Access-Control-Allow-Origin': '*', // Allow all origins
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // Allowed methods
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
+    'Access-Control-Allow-Credentials': 'true', // Allow cookies
+  },
+});
